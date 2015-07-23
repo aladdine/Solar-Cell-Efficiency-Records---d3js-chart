10302cf5b3020dc1a490bb64d9ce46fe6786636c
Alaeddine Mokri <alaeddinemokri@gmail.com>
2015-07-23 01:36:17 -0700

    updated the data structure to include more data points

---
+++ solar_cell_data_updated.js
@@ -0,0 +1,13 @@
+var data = [
+{classification: "Silicon",
+name: "Si (crystalline)",
+efficiency:	25.6,
+efficiency_certainty: 0.5,
+area: 143.7,
+voc: 0.740,
+jsc: 41.8,
+ff: 82.7,
+center: "AIST (2/14)",
+reference: "Panasonic HIT, rear junction [21]"
+}];
+	
