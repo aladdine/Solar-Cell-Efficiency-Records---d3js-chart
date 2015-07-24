cd7a82b92346d48a6dd162574fb19502fe078157
Alaeddine Mokri <alaeddinemokri@gmail.com>
2015-07-24 11:45:17 -0700

    fixed formatting

--- solar_cell_data_updated.js
+++ solar_cell_data_updated.js
@@ -1,13 +1,104 @@
 var data = [
-{classification: "Silicon",
-name: "Si (crystalline)",
-efficiency:	25.6,
-efficiency_certainty: 0.5,
-area: 143.7,
-voc: 0.740,
-jsc: 41.8,
-ff: 82.7,
-center: "AIST (2/14)",
-reference: "Panasonic HIT, rear junction [21]"
-}];
-	
+	{ multijunction: {
+		classification: "Silicon", 
+		name: "Si (crystalline)", 
+		efficiency: 25.6, 
+		certainty: 0.5, 
+		area: 143.7, 
+		voc: 0.740, 
+		jsc: 41.8, 
+		ff: 82.7, 
+		center: "AIST (2/14)", 
+		reference: "Panasonic HIT, rear junction [21]"
+	}, 
+	Crystalline_Si: 16, Thin_Film: 10, Emerging_PV: 3, year: 1975, lab: "NREL"},
+	{multijunction: {
+		classification: "Silicon", 
+		name: "Si (crystalline)", 
+		efficiency: 25.6, 
+		certainty: 0.5, 
+		area: 143.7, 
+		voc: 0.740, 
+		jsc: 41.8, 
+		ff: 82.7, 
+		center: "AIST (2/14)", 
+		reference: "Panasonic HIT, rear junction [21]"
+	}, Crystalline_Si: 17, Thin_Film: 11, Emerging_PV: 4, year: 1985, lab: "NREL"},
+	{ multijunction: {
+		classification: "Silicon", 
+		name: "Si (crystalline)", 
+		efficiency: 25.6, 
+		certainty: 0.5, 
+		area: 143.7, 
+		voc: 0.740, 
+		jsc: 41.8, 
+		ff: 82.7, 
+		center: "AIST (2/14)", 
+		reference: "Panasonic HIT, rear junction [21]"
+	}, 
+	Crystalline_Si: 16, Thin_Film: 10, Emerging_PV: 3, year: 1995, lab: "NREL"},
+	{ multijunction: {
+		classification: "Silicon", 
+		name: "Si (crystalline)", 
+		efficiency: 25.6, 
+		certainty: 0.5, 
+		area: 143.7, 
+		voc: 0.740, 
+		jsc: 41.8, 
+		ff: 82.7, 
+		center: "AIST (2/14)", 
+		reference: "Panasonic HIT, rear junction [21]"
+	}, Crystalline_Si: 17, Thin_Film: 11, Emerging_PV: 4, year: 2000, lab: "NREL"},
+	{ multijunction: {
+		classification: "Silicon", 
+		name: "Si (crystalline)", 
+		efficiency: 25.6, 
+		certainty: 0.5, 
+		area: 143.7, 
+		voc: 0.740, 
+		jsc: 41.8, 
+		ff: 82.7, 
+		center: "AIST (2/14)", 
+		reference: "Panasonic HIT, rear junction [21]"
+	}, 
+	Crystalline_Si: 16, Thin_Film: 10, Emerging_PV: 3, year: 2005, lab: "NREL"},
+	{multijunction: {
+		classification: "Silicon", 
+		name: "Si (crystalline)", 
+		efficiency: 25.6, 
+		certainty: 0.5, 
+		area: 143.7, 
+		voc: 0.740, 
+		jsc: 41.8, 
+		ff: 82.7, 
+		center: "AIST (2/14)", 
+		reference: "Panasonic HIT, rear junction [21]"
+	}, Crystalline_Si: 17, Thin_Film: 11, Emerging_PV: 4, year: 2010, lab: "NREL"},
+	{ multijunction: {
+		classification: "Silicon", 
+		name: "Si (crystalline)", 
+		efficiency: 30, 
+		certainty: 0.5, 
+		area: 143.7, 
+		voc: 0.740, 
+		jsc: 41.8, 
+		ff: 82.7, 
+		center: "AIST (2/14)", 
+		reference: "Panasonic HIT, rear junction [21]"
+	}, 
+	Crystalline_Si: 16, Thin_Film: 10, Emerging_PV: 3, year: 1012, lab: "NREL"},
+	{multijunction: {
+		classification: "Silicon", 
+		name: "Si (crystalline)", 
+		efficiency: 25.6, 
+		certainty: 0.5, 
+		area: 143.7, 
+		voc: 0.740, 
+		jsc: 41.8, 
+		ff: 82.7, 
+		center: "AIST (2/14)", 
+		reference: "Panasonic HIT, rear junction [21]"
+	}, Crystalline_Si: 17, Thin_Film: 11, Emerging_PV: 4, year: 1015, lab: "NREL"}
+]; 
+
+
